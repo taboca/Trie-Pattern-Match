2bc96bb7fc8c9d343cd04691a72119ff2da25193
Marcio <taboca@gmail.com>
2016-07-31 18:12:11 -0300

    The trie grap as a module library

--- test4_graph.js
+++ test4_graph.js
@@ -32,32 +32,5 @@ var trie = {
   }
 } 
 
-var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream(process.argv[2])
-});
-
-var c = 0;
-var i = 0;
-
-trie.init();
-
-lineReader.on('line', function (line) {
-  if(i==0) { 
-    c = parseInt(line); 
-    console.log('Line from file:', c);
-  } else { 
-    trie.update(line);
-  } 
-  i++;
-});
-
-lineReader.on('close', function (line) {
-for(var k in trie.flatArray) { 
-    for(var item in trie.flatArray[k]) { 
-	var target = trie.flatArray[k][item];
- 	console.log(k+"->"+target+":"+item);
-    } 
-}
-});
-
+module.exports = trie;
 
